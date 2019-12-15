815917b7f6dffeb4cdf35d0908c3cfd3048408c3
sveitser <sveitser@gmail.com>
2019-12-15 11:27:17 +0800

    Update system

--- overlays.nix
+++ overlays.nix
@@ -1,12 +1,3 @@
 self: super: {
-  lorri =
-    let src =
-      (super.fetchFromGitHub {
-        owner = "sveitser";
-        repo = "lorri";
-        rev = "a8317c4a05bd06eb1dccb696a28a290b17f6ecb7";
-        sha256 = "1vlj36d2cirbkzp0yzs1rq345zq4l93dxkfp3vnh2x4hm1mlhqwg";
-      });
-    in super.callPackage src { inherit src; };
   kaiti = super.callPackage ./fonts/kaiti/default.nix {};
 }
