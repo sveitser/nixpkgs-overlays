eeb1a2fd5a1cd482501e312fb9cf92419842f340
sveitser <sveitser@gmail.com>
2020-09-08 19:34:57 +0800

    xontrib-fzf-widgets: pypi -> github master

--- python/xontrib-fzf-widgets/default.nix
+++ python/xontrib-fzf-widgets/default.nix
@@ -1,5 +1,5 @@
 { buildPythonPackage
-, fetchPypi
+, fetchFromGitHub
 }:
 
 buildPythonPackage rec {
@@ -7,11 +7,11 @@ buildPythonPackage rec {
   pname = "xontrib-fzf-widgets";
   version = "0.0.3";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "16x83scy8b27mh03aspra2dzsbx6ra2ym26h72vdpr2i5s0cxd2c";
+  src = fetchFromGitHub {
+    owner = "laloch";
+    repo = "xontrib-fzf-widgets";
+    rev = "62aacd796c96b9ac9682b57d165105419c162c8d";
+    sha256 = "1bv7k0i2m0zz0qpq8cv194iri44p2m3b9md5jvwkygcq8xwipk1p";
+    fetchSubmodules = false;
   };
-
-  propagatedBuildInputs = [
-  ];
 }
